49ae7f7c6b10ebd9fdbdffaa6ed8879852e7e96b
Donald2023-source <donalddyusuf@gmail.com>
2025-01-28 11:44:55 -0800

    Changes

--- Main.py
+++ Main.py
@@ -129,7 +129,7 @@ dictionaries = {
 # Search Word Function
 def search_word():
     selected_dictionary = dictionary_var.get()
-    word = entry.get().strip().lower()  # Get user input, trim spaces, and convert to lowercase
+    word = entry.get().strip().lower()  
     meaning = dictionaries.get(selected_dictionary, {}).get(word)
 
     if meaning:
@@ -148,29 +148,29 @@ welcome_label = tk.Label(root, text="Welcome to our Dictionary", padx=200, pady=
 welcome_label.pack()
 root.geometry("600x600")
 
-# Title
+
 title_label = tk.Label(root, text="Please select a Language")
 title_label.pack(pady=10)
 
-#Sekection menu
+
 dictionary_var = tk.StringVar(value="French")
 dictionary_menu = tk.OptionMenu(root, dictionary_var, *dictionaries.keys())
 dictionary_menu.pack()
 
-# Word Entry
+
 word_label = tk.Label(root, text="Enter a word:")
 word_label.pack()
 
 entry = tk.Entry(root)
 entry.pack()
 
-# Search Button
+
 search_button = tk.Button(root, text="Search", command=search_word, pady=10, padx=10)
 search_button.pack(pady=10)
 
-# Result Label
+
 result_label = tk.Label(root, text="Meaning will appear here.")
 result_label.pack(pady=20)
 
-# Run the application
+
 root.mainloop()
